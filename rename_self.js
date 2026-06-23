a81e9c39f694ef3754a70573c3a535be9037a616
test <10583477+0xwzluo@users.noreply.github.com>
2026-06-23 16:31:17 +0800

    Enhance functionality with new ipapi parameter
    Added new parameter 'ipapi' to enhance functionality for querying proxy IP regions using an online API. Updated relevant functions to handle the new parameter and its configurations.

--- rename_self.js
+++ rename_self.js
@@ -8,6 +8,11 @@
  * 5) 过滤空键，避免 "" 命中一切
  * 6) ✅ 清理信息节点 clear 默认开启（可通过 clear=false 关闭）
  * 7) ✅ boolArg 修复：把空字符串/空白当作“未传入”，默认值不被错误覆盖
+ * 8) ✅ ++功能增强：新增参数ipapi（参数格式：策略参数[,在线ip查询api地址,解析结果取值jsonpath表达式]，支持指定ip查询api策略,可通过在线api查询proxy ip所属国家/地区，默认关闭，支持对未匹配部分节点启用、对所有节点启用。策略参数说明：
+ *       关闭：off/留空/其它文本，示例：off  ->  ipapi=off
+ *       对名称未匹配到属地策略的节点启用：nm/on，此时nm参数无效，示例：nm,https://api.ip2location.io/?ip=${ip},$.country_code  ->  ipapi=nm%2Chttps%3A%2F%2Fapi.ip2location.io%2F%3Fip%3D%24%7Bip%7D%2C%24.country_code
+ *       对所有节点启用：all，此时nm参数无效，并且会覆盖通过名称匹配到国家/地区的，示例：all,https://api.ip2location.io/?ip=${ip},$.country_code  ->  ipapi=all%2Chttps%3A%2F%2Fapi.ip2location.io%2F%3Fip%3D%24%7Bip%7D%2C%24.country_code
+ 
  */  
 
 const inArg = $arguments;
@@ -51,13 +56,14 @@ const FGF = inArg.fgf == undefined ? " " : decodeURI(inArg.fgf),
       blockquic = inArg.blockquic == undefined ? "" : decodeURI(inArg.blockquic),
       nameMap = { cn: "cn", zh: "cn", us: "us", en: "us", quan: "quan", gq: "gq", flag: "gq" },
       inname = nameMap[inArg.in] || "",
-      outputName = nameMap[inArg.out] || "";
+      outputName = nameMap[inArg.out] || "",
+      ipapi = resolveIpApiConfigObject(inArg.ipapi || "");
 
 // ==================== 数据表 ====================
 // prettier-ignore
 const FG = ['🇭🇰','🇲🇴','🇹🇼','🇯🇵','🇰🇷','🇸🇬','🇺🇸','🇬🇧','🇫🇷','🇩🇪','🇦🇺','🇦🇪','🇦🇫','🇦🇱','🇩🇿','🇦🇴','🇦🇷','🇦🇲','🇦🇹','🇦🇿','🇧🇭','🇧🇩','🇧🇾','🇧🇪','🇧🇿','🇧🇯','🇧🇹','🇧🇴','🇧🇦','🇧🇼','🇧🇷','🇻🇬','🇧🇳','🇧🇬','🇧🇫','🇧🇮','🇰🇭','🇨🇲','🇨🇦','🇨🇻','🇰🇾','🇨🇫','🇹🇩','🇨🇱','🇨🇴','🇰🇲','🇨🇬','🇨🇩','🇨🇷','🇭🇷','🇨🇾','🇨🇿','🇩🇰','🇩🇯','🇩🇴','🇪🇨','🇪🇬','🇸🇻','🇬🇶','🇪🇷','🇪🇪','🇪🇹','🇫🇯','🇫🇮','🇬🇦','🇬🇲','🇬🇪','🇬🇭','🇬🇷','🇬🇱','🇬🇹','🇬🇳','🇬🇾','🇭🇹','🇭🇳','🇭🇺','🇮🇸','🇮🇳','🇮🇩','🇮🇷','🇮🇶','🇮🇪','🇮🇲','🇮🇱','🇮🇹','🇨🇮','🇯🇲','🇯🇴','🇰🇿','🇰🇪','🇰🇼','🇰🇬','🇱🇦','🇱🇻','🇱🇧','🇱🇸','🇱🇷','🇱🇾','🇱🇹','🇱🇺','🇲🇰','🇲🇬','🇲🇼','🇲🇾','🇲🇻','🇲🇱','🇲🇹','🇲🇷','🇲🇺','🇲🇽','🇲🇩','🇲🇨','🇲🇳','🇲🇪','🇲🇦','🇲🇿','🇲🇲','🇳🇦','🇳🇵','🇳🇱','🇳🇿','🇳🇮','🇳🇪','🇳🇬','🇰🇵','🇳🇴','🇴🇲','🇵🇰','🇵🇦','🇵🇾','🇵🇪','🇵🇭','🇵🇹','🇵🇷','🇶🇦','🇷🇴','🇷🇺','🇷🇼','🇸🇲','🇸🇦','🇸🇳','🇷🇸','🇸🇱','🇸🇰','🇸🇮','🇸🇴','🇿🇦','🇪🇸','🇱🇰','🇸🇩','🇸🇷','🇸🇿','🇸🇪','🇨🇭','🇸🇾','🇹🇯','🇹🇿','🇹🇭','🇹🇬','🇹🇴','🇹🇹','🇹🇳','🇹🇷','🇹🇲','🇻🇮','🇺🇬','🇺🇦','🇺🇾','🇺🇿','🇻🇪','🇻🇳','🇾🇪','🇿🇲','🇿🇼','🇦🇩','🇷🇪','🇵🇱','🇬🇺','🇻🇦','🇱🇮','🇨🇼','🇸🇨','🇦🇶','🇬🇮','🇨🇺','🇫🇴','🇦🇽','🇧🇲','🇹🇱']
 // prettier-ignore
-const EN = ['HK','MO','TW','JP','KR','SG','US','GB','FR','DE','AU','AE','AF','AL','DZ','AO','AR','AM','AT','AZ','BH','BD','BY','BE','BZ','BJ','BT','BO','BA','BW','BR','VG','BN','BG','BF','BI','KH','CM','CA','CV','KY','CF','TD','CL','CO','KM','CG','CD','CR','HR','CY','CZ','DK','DJ','DO','EC','EG','SV','GQ','ER','EE','ET','FJ','FI','GA','GM','GE','GH','GR','GL','GT','GN','GY','HT','HN','HU','IS','IN','ID','IR','IQ','IE','IM','IL','IT','CI','JM','JO','KZ','KE','KW','KG','LA','LV','LB','LS','LR','LY','LT','LU','MK','MG','MW','MY','MV','ML','MT','MR','MU','MX','MD','MC','MN','ME','MA','MZ','MM','NA','NP','NL','NZ','NI','NE','NG','KP','NO','OM','PK','PA','PY','PE','PH','PT','PR','QA','RO','RU','RW','SM','SA','SN','RS','SL','SK','SI','SO','ZA','ES','LK','SD','SR','SZ','SE','CH','SY','TJ','TZ','TH','TG','TO','TT','TN','TR','TM','VI','UG','UA','UY','UZ','VE','VN','YE','ZM','ZW','AD','RE','PL','GU','VA','LI','CW','SC','AQ','GI','CU','FO','AX','BM','TL'];
+const EN = ['HK','MO','TW','JP','KR','SG','US','GB','FR','DE','AU','AE','AF','AL','DZ','AO','AR','AM','AT','AZ','BH','BD','BY','BE','BZ','BJ','BT','BO','BA','BW','BR','VG','BN','BG','BF','BI','KH','CM','CA','CV','KY','CF ','TD','CL','CO','KM','CG','CD','CR','HR','CY','CZ','DK','DJ','DO','EC','EG','SV','GQ','ER','EE','ET','FJ','FI','GA','GM','GE','GH','GR','GL','GT','GN','GY','HT','HN','HU','IS','IN','ID','IR','IQ','IE','IM','IL','IT','CI','JM','JO','KZ','KE','KW','KG','LA','LV','LB','LS','LR','LY','LT','LU','MK','MG','MW','MY','MV','ML','MT','MR','MU','MX','MD','MC','MN','ME','MA','MZ','MM','NA','NP','NL','NZ','NI','NE','NG','KP','NO','OM','PK','PA','PY','PE','PH','PT','PR','QA','RO','RU','RW','SM','SA','SN','RS','SL','SK','SI','SO','ZA','ES','LK','SD','SR','SZ','SE','CH','SY','TJ','TZ','TH','TG','TO','TT','TN','TR','TM','VI','UG','UA','UY','UZ','VE','VN','YE','ZM','ZW','AD','RE','PL','GU','VA','LI','CW','SC','AQ','GI','CU','FO','AX','BM','TL'];
 // prettier-ignore
 const ZH = ['香港','澳门','台湾','日本','韩国','新加坡','美国','英国','法国','德国','澳大利亚','阿联酋','阿富汗','阿尔巴尼亚','阿尔及利亚','安哥拉','阿根廷','亚美尼亚','奥地利','阿塞拜疆','巴林','孟加拉国','白俄罗斯','比利时','伯利兹','贝宁','不丹','玻利维亚','波斯尼亚和黑塞哥维那','博茨瓦纳','巴西','英属维京群岛','文莱','保加利亚','布基纳法索','布隆迪','柬埔寨','喀麦隆','加拿大','佛得角','开曼群岛','中非共和国','乍得','智利','哥伦比亚','科摩罗','刚果(布)','刚果(金)','哥斯达黎加','克罗地亚','塞浦路斯','捷克','丹麦','吉布提','多米尼加共和国','厄瓜多尔','埃及','萨尔瓦多','赤道几内亚','厄立特里亚','爱沙尼亚','埃塞俄比亚','斐济','芬兰','加蓬','冈比亚','格鲁吉亚','加纳','希腊','格陵兰','危地马拉','几内亚','圭亚那','海地','洪都拉斯','匈牙利','冰岛','印度','印尼','伊朗','伊拉克','爱尔兰','马恩岛','以色列','意大利','科特迪瓦','牙买加','约旦','哈萨克斯坦','肯尼亚','科威特','吉尔吉斯斯坦','老挝','拉脱维亚','黎巴嫩','莱索托','利比里亚','利比亚','立陶宛','卢森堡','马其顿','马达加斯加','马拉维','马来','马尔代夫','马里','马耳他','毛利塔尼亚','毛里求斯','墨西哥','摩尔多瓦','摩纳哥','蒙古','黑山共和国','摩洛哥','莫桑比克','缅甸','纳米比亚','尼泊尔','荷兰','新西兰','尼加拉瓜','尼日尔','尼日利亚','朝鲜','挪威','阿曼','巴基斯坦','巴拿马','巴拉圭','秘鲁','菲律宾','葡萄牙','波多黎各','卡塔尔','罗马尼亚','俄罗斯','卢旺达','圣马力诺','沙特阿拉伯','塞内加尔','塞尔维亚','塞拉利昂','斯洛伐克','斯洛文尼亚','索马里','南非','西班牙','斯里兰卡','苏丹','苏里南','斯威士兰','瑞典','瑞士','叙利亚','塔吉克斯坦','坦桑尼亚','泰国','多哥','汤加','特立尼达和多巴哥','突尼斯','土耳其','土库曼斯坦','美属维尔京群岛','乌干达','乌克兰','乌拉圭','乌兹别克斯坦','委内瑞拉','越南','也门','赞比亚','津巴布韦','安道尔','留尼汪','波兰','关岛','梵蒂冈','列支敦士登','库拉索','塞舌尔','南极','直布罗陀','古巴','法罗群岛','奥兰群岛','百慕达','东帝汶'];
 // prettier-ignore
@@ -201,9 +207,11 @@ function operator(pro) {
 
   const BLKEYS = BLKEY ? BLKEY.split("+") : "";
 
+  // new feature:ipapi预处理
+  prerenameProxysByIpRegion(ipapi,pro);
+
   pro.forEach((e) => {
     let bktf = false, ens = e.name; // ens: 原始名称（本轮处理前）
-
     // 预处理（归一化 + 可选 BLKEY 附加到显示）
     Object.keys(rurekey).forEach((ikey) => {
       if (rurekey[ikey].test(e.name)) {
@@ -281,7 +289,15 @@ function operator(pro) {
       keyover = keyover.concat(firstName, usflag, nNames, findKeyValue, retainKey, ikey, ikeys).filter(Boolean);
       e.name = keyover.join(FGF);
     } else {
-      if (nm) e.name = FNAME + FGF + e.name; else e.name = null;
+      //未匹配到地区，判断是否要通过ipapi查询
+      if (nm||ipapi.strategy=="on") {
+        if(ipapi.strategy=="on"){
+          renameProxyByIpRegion(ipapi,e);
+        }
+        e.name = FNAME + FGF + e.name; 
+      } else {
+        e.name = null;
+      }
     }
   });
 
@@ -337,4 +353,124 @@ function fampx(pro){
   wis.sort((a,b)=> sps[wis.indexOf(a)]-sps[wis.indexOf(b)] || a.name.localeCompare(b.name));
   wnout.sort((a,b)=> pro.indexOf(a)-pro.indexOf(b));
   return wnout.concat(wis);
+}
+
+
+const childProcess = require("child_process"); //使用子进程模块
+//const workThreads = require("worker_threads"); //使用多线程模块
+const regionCaches={}; //缓存对象
+function resolveIpApiConfigObject(ipApiparam){
+    if(!ipApiparam){
+        return {"strategy":"off"}
+    }
+    ipApiparam=decodeURIComponent(ipApiparam.trim()).trim().split(",");
+    return {
+        "jsonpath":ipApiparam.pop()||"",
+        "apiUrl":ipApiparam.pop()||"",
+        "strategy":ipApiparam.pop()||"off"
+    };
+}
+function saveCache(key,value){
+    //更新缓存
+    prev = regionCaches[key];
+    regionCaches[key]=value;
+    return prev;
+}
+function getCache(key){
+    //获取缓存
+    return regionCaches[key];
+}
+function valueOfPath(jsonObject,arr){
+    //jsonpath值提取
+    return arr&&arr.length?valueOfPath(jsonObject[arr.pop()],arr):jsonObject;
+}
+function validIp(host){
+    //ip格式检查
+    return /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(host);
+}
+function validDomain(host){
+    //域名格式检查
+    return /^(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}$/.test(host);
+}
+function retriveIpv4(nsPost){
+    //ipv4串提取
+    return nsPost.match(/(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/g).pop();
+}
+function curlGet(url){
+    try {
+        //使用系统命令curl发送请求
+        return childProcess.execSync(`curl ${url}`).toString('utf-8');
+    } catch (error) {
+        console.error("curl执行失败:", error.message);
+        return "";
+    }
+}
+function resolveDomain(domain) {
+    try {
+        // 使用系统命令nslookup解析domain为ip
+        return retriveIpv4(childProcess.execSync(`nslookup ${domain}`).toString('utf-8'));
+    } catch (error) {
+        console.error("域名解析失败:", error.message);
+        return "";
+    }
+}
+function ensureIp(host){
+    //确保ip格式，如果是域名，则将域名解析为ip
+    if(validDomain(host)){
+        return resolveDomain(host);
+    }
+    return host;
+}
+function resolveHostRegion(host,apiurl,ipvalPattern) {
+    try {
+        //host格式检查
+        host=host.trim();
+        //优化：先查cache，命中则不再查询
+        let cache = getCache(host);
+        if(cache){
+            return cache;
+        }
+        let originHost = host;
+        host = ensureIp(host);
+        if(!validIp(host)){
+            console.error("非法ip串：", host);
+            return "";
+        }
+        apiurl=apiurl.replace("${ip}",host);
+        object = JSON.parse(curlGet(apiurl));
+        let regionResult = valueOfPath(object,ipvalPattern.trim().substring(2).split(".").reverse());
+        saveCache(originHost,regionResult);
+        return regionResult;
+    } catch (error) {
+        console.error("数据请求失败：", error.message);
+        return "";
+    }
+}
+
+function renameProxyByIpRegion(ipapiConfig,proxy) {
+    //重命名处理
+    try {
+        const regionName = resolveHostRegion(proxy.server,ipapiConfig.apiUrl,ipapiConfig.jsonpath);
+        console.log(`解析到节点区域:${regionName}`);
+        regionName && (proxy.name=regionName);
+    } catch (error) {
+        console.error(`节点[${proxy.name || proxy.server}]预重命名处理失败：${error.message}`);
+    }
+    return proxy;
+}
+
+
+function prerenameProxysByIpRegion(ipapiConfig,proxys) {
+    //预先重命名处理
+    try {
+        if(ipapiConfig.strategy=="all"){
+            console.log(`节点处理strategy:${ipapiConfig.strategy}`);
+            proxys.forEach(x => {
+                renameProxyByIpRegion(ipapiConfig,x);
+            });
+        }
+    } catch (error) {
+        console.error("预先重命名处理失败：", error.message);
+    }
+    return proxys;
 }
